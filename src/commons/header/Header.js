aaa6de8214e1f7e3cf0695bd32798640586d0dc3
etechcollab <e.techcollab@gmail.com>
2021-04-25 16:05:13 +0530

    Modal and Form Code Update

--- src/commons/header/Header.js
+++ src/commons/header/Header.js
@@ -1,18 +1,58 @@
 import React, { Component } from 'react';
 import './Header.css';
 import Button from '@material-ui/core/Button';
+import logo from '../../assets/logo.svg';
+import Modal from 'react-modal';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
 class Header extends Component {
-    render() {
+    
+    constructor()
+{
+    super();
+    this.state = {
+        modalIsOpen: false,
+        value: 0
+
+    }
+}
+
+openModelHandler= () =>{this.setState=({modalIsOpen: true})}
+
+closeModalHandler = () =>{this.setState=({modalIsOpen: false})}
+
+
+
+handleChangeHandler= (event, value) => {this.setState=({value})}
+
+
+
+render() {
         return (
             <div>
                
                 <header className="app-header">
+                    <img src={logo} className="app-logo" alt="logo"/>
                     <div className="login-button">
-                        <Button variant="contained" color="default">
+                        <Button variant="contained" color="default" onClick= {this.openModelHandler} >
                             Login
                         </Button>
                     </div>
                 </header>
+<Modal ariaHideApp={false} isOpen={this.state.modalIsOpen} contentLabel="Login" onRequestClose={this.closeModalHandler}>
+
+
+<Tabs value= {this.state.value} onChange={this.handleChangeHandler} >
+
+<Tab label="Login"/>
+
+<Tab label="Register"/>
+
+
+</Tabs>
+
+
+</Modal>
             </div>
         )
     }
