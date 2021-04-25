e859bade225ee889a23c6c0c7585ef776e36bddd
etechcollab <e.techcollab@gmail.com>
2021-04-25 11:41:21 +0530

    Cleaned code and added folder structure

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+//import registerServiceWorker from './registerServiceWorker';
+
+
+ReactDOM.render( <div></div> , document.getElementById('root'));
+
+
