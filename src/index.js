ad56aed07a8fea487463dcb558d7d60a50486723
etechcollab <e.techcollab@gmail.com>
2021-04-25 11:59:17 +0530

    Added Homepage

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-//import registerServiceWorker from './registerServiceWorker';
+import registerServiceWorker from './registerServiceWorker';
+import Home from './screens/Home/home';
 
 
-ReactDOM.render( <div></div> , document.getElementById('root'));
+ReactDOM.render( <Home/> , document.getElementById('root'));
+
+registerServiceWorker();
 
 
