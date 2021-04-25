1dfd3cac4dbda84f42f18444e47a18ffd049a036
etechcollab <e.techcollab@gmail.com>
2021-04-25 18:18:08 +0530

    username and password added on to the Form

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 //import registerServiceWorker from './registerServiceWorker';
 import Home from './screens/Home/home';
+import 'fontsource-roboto';
 
 
 ReactDOM.render( <Home/> , document.getElementById('root'));
